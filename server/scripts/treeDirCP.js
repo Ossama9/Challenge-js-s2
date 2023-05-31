f19916673babe8cd7b189bd383e4f2cd0e69c5aa
Ossama9 <oussama.dahbi98@gmail.com>
2023-05-31 22:42:27 +0200

    create vue app

--- server/scripts/treeDirCP.js
+++
@@ -1,9 +0,0 @@
-const childProcess = require("child_process");
-
-childProcess.exec("ls -lR", (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(stdout);
-});
--- server/server/routes/user.js
+++
@@ -1,5 +0,0 @@
-const genericRouter = require("./generic");
-const genericController = require("../controllers/generic");
-const UserService = require("../services/user");
-
-module.exports = new genericRouter(new genericController(new UserService()));
